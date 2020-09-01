202582f80c431cff2efeb7be78fd53541502ca56
Elbie-em <emoonga1993@gmail.com>
2020-09-01 22:50:32 +0200

    Preload Assets

--- js/SceneMainMenu.js
+++ js/SceneMainMenu.js
@@ -1,9 +1,21 @@
 class SceneMainMenu extends Phaser.Scene {
-	constructor() {
-		super({ key: "SceneMainMenu" });
-	}
+  constructor() {
+    super({ key: "SceneMainMenu" });
+  }
 
-	create() {
-		this.scene.start("SceneMain");
-	}
+  preload() {
+    this.load.image("sprBtnPlay", "res/sprBtnPlay.png");
+    this.load.image("sprBtnPlayHover", "res/sprBtnPlayHover.png");
+    this.load.image("sprBtnPlayDown", "res/sprBtnPlayDown.png");
+    this.load.image("sprBtnRestart", "res/sprBtnRestart.png");
+    this.load.image("sprBtnRestartHover", "res/sprBtnRestartHover.png");
+    this.load.image("sprBtnRestartDown", "res/sprBtnRestartDown.png");
+
+    this.load.audio("sndBtnOver", "res/sndBtnOver.wav");
+    this.load.audio("sndBtnDown", "res/sndBtnDown.wav");
+  }
+
+  create() {
+    this.scene.start("SceneMain");
+  }
 }
