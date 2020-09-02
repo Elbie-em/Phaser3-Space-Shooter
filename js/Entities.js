06d3d8cc0b60ab1e9887f118a08e18c00d446c2c
Elbie-em <emoonga1993@gmail.com>
2020-09-02 18:58:31 +0200

    Add velocity for span location of enemy classes

--- js/Entities.js
+++ js/Entities.js
@@ -42,20 +42,23 @@ class Player extends Entity {
 
 class ChaserShip extends Entity {
 	constructor(scene, x, y) {
-	  super(scene, x, y, "sprEnemy1", "ChaserShip");
+		super(scene, x, y, "sprEnemy1", "ChaserShip");
+		this.body.velocity.y = Phaser.Math.Between(50, 100);
 	}
 }
 
 class GunShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy0", "GunShip");
-    this.play("sprEnemy0");
+		this.play("sprEnemy0");
+		this.body.velocity.y = Phaser.Math.Between(50, 100);
   }
 }
 
 class CarrierShip extends Entity {
   constructor(scene, x, y) {
     super(scene, x, y, "sprEnemy2", "CarrierShip");
-    this.play("sprEnemy2");
+		this.play("sprEnemy2");
+		this.body.velocity.y = Phaser.Math.Between(50, 100);
   }
 }
