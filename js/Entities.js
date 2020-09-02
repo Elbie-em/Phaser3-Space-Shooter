e60da46d4d0d847a80ff8c11f9aa2c7701986593
Elbie-em <emoonga1993@gmail.com>
2020-09-02 18:47:30 +0200

    Add CarrirerShip class

--- js/Entities.js
+++ js/Entities.js
@@ -52,3 +52,10 @@ class GunShip extends Entity {
     this.play("sprEnemy0");
   }
 }
+
+class CarrierShip extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprEnemy2", "CarrierShip");
+    this.play("sprEnemy2");
+  }
+}
