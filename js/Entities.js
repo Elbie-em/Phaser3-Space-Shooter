5b2cc0ec174ac16f044e59c2a18e4cbcd221f6ca
Elbie-em <emoonga1993@gmail.com>
2020-09-02 23:46:30 +0200

    Add update function to Scrolling backgrounf

--- js/Entities.js
+++ js/Entities.js
@@ -211,4 +211,13 @@ class ScrollingBackground {
       this.layers.add(layer);
     }
 	}
+
+	update() {
+		if (this.layers.getChildren()[0].y > 0) {
+      for (var i = 0; i < this.layers.getChildren().length; i++) {
+        var layer = this.layers.getChildren()[i];
+        layer.y = (-layer.displayHeight) + (layer.displayHeight * i);
+      }
+    }
+	}
 }
