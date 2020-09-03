e9b1c6df99dcc8451aeb91ac771cf088eb718843
Elbie-em <emoonga1993@gmail.com>
2020-09-03 19:51:11 +0200

    Start build on Game over scene

--- js/SceneGameOver.js
+++ js/SceneGameOver.js
@@ -4,6 +4,13 @@ class SceneGameOver extends Phaser.Scene {
 	}
 
 	create() {
-
+		this.title = this.add.text(this.game.config.width * 0.5, 128, "GAME OVER", {
+      fontFamily: 'monospace',
+      fontSize: 48,
+      fontStyle: 'bold',
+      color: '#ffffff',
+      align: 'center'
+    });
+    this.title.setOrigin(0.5);
 	}
 }
